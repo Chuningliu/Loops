2461463a3eda19dd9092f87820f59939a31d94e1
Chuningliu <chuningliukahmei@gmail.com>
2021-12-23 10:20:23 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,34 @@
+# index = 1
+# while(index < 10):
+#     print(index)
+#     index = index + 1
+
+
+# i = 1
+# sum=0
+# while i <= 140:
+#     if i % 3 == 0:
+#         sum = sum + i
+#         print(sum)
+#     i+=1
+
+# i=0
+# while(i<10):
+#     j = 0
+#     while(j<=5):
+#         print(j)
+#         j = j + 1
+#         i = i + 1
+#     print(i)
+
+
+# i = 0
+# num = int(input("Enter your number:- "))
+# while(i <= num):
+#     if(num > 0):
+#         print("it is positive")
+#     elif(num < 0):
+#         print("it is negative")
+#     else :
+#         print("zero")
+#     i = i + 1 
